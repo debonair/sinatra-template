e2c97432249ea8d150dc9085050bf3bd397d4f22
duma <duma@whitewallweb.com>
2013-04-02 15:51:39 +0200

    Added some upload functionality to the site

---
+++ routes/server.rb
@@ -0,0 +1,17 @@
+class MyApp < Sinatra::Base
+
+  # Handle GET-request (Show the upload form)
+	get "/upload" do
+	  haml :upload
+	end      
+	    
+	# Handle POST-request (Receive and save the uploaded file)
+	post "/upload" do 
+	  File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+	    f.write(params['myfile'][:tempfile].read)
+	  end
+	  return "The file was successfully uploaded!"
+	end
+
+end
+
